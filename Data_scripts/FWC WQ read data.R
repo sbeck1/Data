1a90008dad6ca41ad4d193d993a59e76bc2f23e6
Erica Christensen <erica.christensen@weecology.org>
2018-02-12 20:41:06 -0500

    convert .sas7bdat files to .csv; included script to do this in 'FWC WQ read data.R'

--- Data_scripts/FWC WQ read data.R
+++ Data_scripts/FWC WQ read data.R
@@ -1,6 +1,6 @@
-#code to read FWC SAS files with water quality data
+#code to read FWC SAS files with water quality data, and convert to csv
 
-setwd("C:/Users/billpine/Google Drive/GEBF Lone Cabbage Oyster/WQ/FWC_FIM")
+#setwd("C:/Users/billpine/Google Drive/GEBF Lone Cabbage Oyster/WQ/FWC_FIM")
 
 #Note from Caleb
 #I have attached a file that shows the structure of our water quality data.  
@@ -9,7 +9,10 @@ setwd("C:/Users/billpine/Google Drive/GEBF Lone Cabbage Oyster/WQ/FWC_FIM")
 
 library(sas7bdat)
 
+# read in water quality data (hydro) and physical data (physical)
+hydro = read.sas7bdat("Cooperator_WQ/ckm_hydrolab.sas7bdat")
+physical = read.sas7bdat("Cooperator_WQ/ckm_physical.sas7bdat")
 
-hydro=read.sas7bdat("ckm_2017_hydrolab.sas7bdat")
-physical=read.sas7bdat("ckm_2017_physical.sas7bdat")
-
+# write to csv
+write.csv(hydro,"Cooperator_WQ/FWC_waterquality.csv",row.names=F,na='',quote=F)
+write.csv(physical,"Cooperator_WQ/FWC_physicaltable.csv",row.names=F,na='',quote=F)
